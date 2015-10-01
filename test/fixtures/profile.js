bb4bce3e43804df283f7f1f6e9e6f172c34ae305
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-01 12:58:51 +0300

    Rewrite all tests with ES6

--- test/fixtures/profile.js
+++ test/fixtures/profile.js
@@ -1,6 +1,6 @@
-{
+export default JSON.stringify({
   "email": "ghaiklor@gmail.com",
   "postal_code": "94703-1111",
   "name": "Eugene Obrezkov",
   "user_id": "amzn1.account.XXX00XXXXXXXXXX0XXXXXXXXXXXX"
-}
+});
